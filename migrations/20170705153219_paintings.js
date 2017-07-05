16397d73aae6755cfa574305eb50fc8852f02a9d
Jon Ramer <jramer87@gmail.com>
2017-07-05 15:35:51 -0500

    Adds paintings migration

---
+++ migrations/20170705153219_paintings.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('paintings', table => {
+    table.increments();
+    table.string('title')
+    table.text('description');
+    table.text('size');
+    table.decimal('price');
+    table.integer('artist_id')
+      .notNullable()
+      .references('id')
+      .inTable('artists')
+      .onDelete('CASCADE')
+      .index();
+  })
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('paintings');
+};
