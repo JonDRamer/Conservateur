2040b9461796e2d9f255b58e3e8924992818ec45
Jon Ramer <jramer87@gmail.com>
2017-07-05 15:19:12 -0500

    Updates users migration to reflect new boolean user group approach

--- migrations/20170705143838_users.js
+++ migrations/20170705143838_users.js
@@ -1,8 +1,21 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', table => {
+    table.increments();
+    table.string('email')
+      .notNullable()
+      .unique();
+    table.string('password_digest')
+      .notNullable();
+    table.string('first_name')
+      .notNullable();
+    table.string('last_name')
+      .notNullable();
+    table.boolean('artist');
+    table.boolean('collector');
+    table.boolean('curator');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users');
 };
