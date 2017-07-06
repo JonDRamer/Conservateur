ae9e1f728a1f80a71b4cdf0b036a7566bba2066c
Jon Ramer <jramer87@gmail.com>
2017-07-06 11:24:46 -0500

    Adds post route to collectors router

--- routes/collectors.js
+++ routes/collectors.js
@@ -11,7 +11,26 @@ router.route('/')
       .catch(err => next(err));
   })
   .post((req, res, next) => {
-
+    knex('users')
+      .insert({
+        email: req.body.email,
+        password_digest: req.body.password_digest,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        artist: req.body.artist,
+        collector: req.body.collector,
+        curator: req.body.curator
+      })
+      .returning('*')
+      .then((users) => {
+        knex('collectors')
+          .insert({
+            bio: req.body.bio,
+            user_id: users[0].id
+          })
+          .returning('*', users)
+          .then(collectors => res.json(`${users[0].first_name}'s Bio: ${collectors[0].bio} was successfully added to the collectors table.`))
+      })
   })
 router.route('/:collector_id')
   .get((req, res, next) => {
