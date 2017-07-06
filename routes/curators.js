d6a99b136381be679bf42728a6ea63154e26e5a1
Jon Ramer <jramer87@gmail.com>
2017-07-06 15:15:16 -0500

    Adds post route to curators router

--- routes/curators.js
+++ routes/curators.js
@@ -10,6 +10,30 @@ router.route('/')
       .then(curators => res.json(curators))
       .catch(err => next(err));
   })
+  .post((req, res, next) => {
+    knex('users')
+      .insert({
+        email: req.body.email,
+        password_digest: req.body.password_digest,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        artist: req.body.artist,
+        collector: req.body.collector,
+        curator: req.body.curator
+      })
+      .returning('*')
+      .then((users) => {
+        knex('curators')
+          .insert({
+            bio: req.body.bio,
+            rating: req.body.rating,
+            user_id: users[0].id
+          })
+          .returning('*', users)
+          .then(curators => res.json(`${users[0].first_name} ${users[0].last_name}'s bio: ${curators[0].bio} and rating: ${curators[0].rating} were successfully added to the curator's table.`))
+          .catch(err => next(err));
+      })
+  })
 
 
 router.route('/:curator_id')
