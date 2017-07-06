cda65fcb6fb655ed2fef7a8de1a908fa0ee5b3f6
Jon Ramer <jramer87@gmail.com>
2017-07-06 14:00:57 -0500

    Adds post route for artists router

--- routes/artists.js
+++ routes/artists.js
@@ -10,6 +10,29 @@ router.route('/')
       .then(artists => res.json(artists))
       .catch(err => next(err));
   })
+  .post((req, res, next) => {
+    knex('users')
+      .insert({
+        email: req.body.email,
+        password_digest: req.body.password_digest,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        artist: req.body.artist,
+        collector: req.body.collector,
+        curator: req.body.curator
+      })
+      .returning('*')
+      .then((users) => {
+        knex('artists')
+          .insert({
+            bio: req.body.bio,
+            user_id: users[0].id
+          })
+          .returning('*', users)
+          .then(artists => res.json(`${users[0].first_name} ${users[0].last_name}'s bio: ${artists[0].bio} was successfully added to the artist's table.`))
+          .catch(err => next(err));
+      })
+  })
 
 router.route('/:artist_id')
 
