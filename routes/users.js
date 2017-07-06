a38b32743bce4710d94fdce4630e6d123be58eae
Jon Ramer <jramer87@gmail.com>
2017-07-06 09:43:38 -0500

    Adds get all and post routes to the users router

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,16 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.route('/')
-  .get((req, res) => {
+  .get((req, res, next) => {
     knex('users')
       .then(users => res.json(users))
+      .catch(err => next(err));
+  })
+  .post((req, res, next) => {
+    knex('users')
+      .insert(req.body)
+      .returning('id')
+      .then((id) => res.json(id))
       .catch(err => next(err));
   });
 
