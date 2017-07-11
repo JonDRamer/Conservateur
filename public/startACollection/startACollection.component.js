e865bcd17a2cf0fc7e60475e44c73e881774e78d
Jon Ramer <jramer87@gmail.com>
2017-07-11 15:55:24 -0500

    Adds submitOrder function to start a collection controller and saves order info to localStorage.order

--- public/startACollection/startACollection.component.js
+++ public/startACollection/startACollection.component.js
@@ -13,6 +13,24 @@
     const vm = this;
 
     vm.onInit = () => {}
+
+    vm.submitOrder = (consultation) => {
+      vm.order = {
+        name: vm.consultation.name.$viewValue,
+        email: vm.consultation.email.$viewValue,
+        city: vm.consultation.city.$viewValue,
+        state: vm.consultation.state.$viewValue,
+        style: vm.consultation.style.$viewValue,
+        url: vm.consultation.url.$viewValue,
+        space: vm.consultation.space.$viewValue,
+        images: vm.consultation.images.$viewValue,
+        budget: vm.consultation.budget.$viewValue,
+        timeline: vm.consultation.timeline.$viewValue
+      }
+      console.log("VM.Order: ", vm.order);
+      localStorage.order = JSON.stringify(vm.order);
+      console.log("Local Storage Order: ", JSON.parse(localStorage.order));
+    }
   }
 
 }());
