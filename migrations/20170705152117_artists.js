29c172a6464a58a56c2c1a0f0ea2e504010bb5b5
Jon Ramer <jramer87@gmail.com>
2017-07-14 14:48:24 -0500

    updates artists column migration and seed

--- migrations/20170705152117_artists.js
+++ migrations/20170705152117_artists.js
@@ -4,6 +4,16 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('artists', table => {
     table.increments();
     table.text('bio');
+    table.text('tags')
+      .notNullable();
+    table.string('img_url1')
+      .notNullable();
+    table.string('img_url2')
+      .notNullable();
+    table.string('img_url3')
+      .notNullable();
+    table.string('img_url4')
+      .notNullable();
     table.integer('user_id')
       .notNullable()
       .references('id')
--- seeds/2-artists.js
+++ seeds/2-artists.js
@@ -8,10 +8,20 @@ exports.seed = function(knex, Promise) {
       return knex('artists')
         .insert([{
             bio: 'Kati grew up in the desert in California.  She developed a love for the desert and has been painint it ever since.',
+            tags: 'landscape, desert, mountains, cactus, cacti, california, prickly pear, southwestern, new mexico, canvas',
+            img_url1: 'http://www.katiramer.com/available-paintings-1/',
+            img_url2: 'http://www.katiramer.com/available-paintings-1/',
+            img_url3: 'http://www.katiramer.com/available-paintings-1/',
+            img_url4: 'http://www.katiramer.com/available-paintings-1/',
             user_id: 1
           },
           {
             bio: 'Katie is basically a really cool painter with lots of talent',
+            tags: 'blue, color, movement, abstract, fluid, water, canvas',
+            img_url1: 'http://www.katiecraigart.com/originals/texas-sunrise-original-canvas',
+            img_url2: 'http://www.katiecraigart.com/originals/betsy-original-canvas',
+            img_url3: 'http://www.katiecraigart.com/originals/quiet-breath-original-framed-canvas',
+            img_url4: 'http://www.katiecraigart.com/originals/in-all-things-original-framed-canvas',
             user_id: 2
           }
         ]);
