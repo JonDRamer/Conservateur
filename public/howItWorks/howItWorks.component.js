ad54bba169a7c9448c4fc13fe0612264aef42e3a
Jon Ramer <jramer87@gmail.com>
2017-07-16 14:47:34 -0500

    Resets question form after submission and deletes controller copy

--- public/howItWorks/howItWorks.component.js
+++ public/howItWorks/howItWorks.component.js
@@ -15,6 +15,20 @@
     vm.onInit = () => {
       vm.form = false;
     }
+
+    vm.sendQuestion = () => {
+      vm.question = {
+        name: vm.consultation.name.$viewValue,
+        email: vm.consultation.email.$viewValue,
+        question: vm.consultation.question.$viewValue
+      }
+      console.log(vm.question);
+      // $http.post("/collectors/question", vm.question);
+      delete vm.question;
+      vm.consultation.$setPristine();
+      vm.form = false;
+
+    }
   }
 
 }());
